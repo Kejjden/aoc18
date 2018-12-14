fbf44dc30a5ba4d3ecd59237f6041ceb919b35ad
Richard Krantz <richard.krantz@mathem.se>
2018-12-14 15:29:15 +0100

    I dare you to make anything worse. This will be the champion of worse

--- day6/part1.py
+++ day6/part1.py
@@ -2,33 +2,27 @@ import string
 import math
 from collections import OrderedDict, defaultdict
 
-def part1(seq):
-	og_points = dict(zip(string.ascii_uppercase, [dict(zip(['x', 'y'], [int(i) for i in x.split(', ')])) for x in seq]))
-	closest = defaultdict(int)
-	points = og_points.copy()
-	hull = quick_hull(points)
+def check_points(seq, overflow):
+	points = dict(zip([str(i) for i in range(0, len(seq))], [dict(
+		zip(
+			['x', 'y', 'size'], 
+			([int(i) for i in x.split(', ')] + [0])
+		)
+	) for x in seq]))
 
-	hull_min_y = hull[min(hull, key=lambda i: hull[i]['y'])]
-	hull_max_y = hull[max(hull, key=lambda i: hull[i]['y'])]
-	hull_min_x = hull[min(hull, key=lambda i: hull[i]['x'])]
-	hull_max_x = hull[max(hull, key=lambda i: hull[i]['x'])]
+	hull_min_y = points[min(points, key=lambda i: points[i]['y'])]["y"]
+	hull_max_y = points[max(points, key=lambda i: points[i]['y'])]["y"]
+	hull_min_x = points[min(points, key=lambda i: points[i]['x'])]["x"]
+	hull_max_x = points[max(points, key=lambda i: points[i]['x'])]["x"]
 	
-	boundry_box = {
-		'tl': {'x': hull_min_x['x'], 'y': hull_min_y['y']},  # top left
-		'tr': {'x': hull_max_x['x'], 'y': hull_min_y['y']},  # top right
-		'br': {'x': hull_min_x['x'], 'y': hull_max_y['y']},  # bottom right
-		'bl': {'x': hull_max_x['x'], 'y': hull_max_y['y']}   # bottom left
-	}
+	for grid_y in range(hull_min_y-overflow, hull_max_y+overflow):
+		for grid_x in range(hull_min_x-overflow, hull_max_x+overflow):
 
-	for grid_y in range(hull_min_y['y'], hull_max_y['y']):
-		for grid_x in range(hull_min_x['x'], hull_max_x['x']):
-
-			shortest_distance = 99999
+			shortest_distance = 9999999
 			shortest_distance_point_index = []
 
-			for point_index in og_points:
-				dist = math.sqrt( (og_points[point_index]['x'] - grid_x)**2 + (og_points[point_index]['y'] - grid_y)**2 )
-
+			for point_index in points:
+				dist = abs(points[point_index]['y'] - grid_y) + abs(points[point_index]['x'] - grid_x)
 				if dist < shortest_distance:
 					shortest_distance = dist
 					shortest_distance_point_index = [point_index]
@@ -36,61 +30,21 @@ def part1(seq):
 					shortest_distance_point_index.append(point_index)
 
 			if len(shortest_distance_point_index) == 1:
-				closest[shortest_distance_point_index[0]] += 1
+				points[shortest_distance_point_index[0]]['size'] += 1
 
+	return points
 
-	maxman = max([x for x in closest if x not in hull.keys()], key=lambda i: closest[i])
+def part1(seq):
+	points = check_points(seq, 0)
+	points2 = check_points(seq, 20)
+	
+	max_zone = 0
+	for pi in points:
+		if points[pi]['size'] == points2[pi]['size'] and points[pi]['size'] > max_zone:
+			max_zone = points[pi]["size"]
+	
+	return max_zone
 
-	return closest[maxman] + 1
-
-def quick_hull(points):
-	hull = {}
-
-	max_x = max(points, key=lambda i: points[i]['x'])
-	min_x = min(points, key=lambda i: points[i]['x'])
-
-	hull[max_x] = points.pop(max_x)
-	hull[min_x] = points.pop(min_x)
-
-	right_of_line = {}
-	left_of_line = {}
-
-	for point in points:
-		side = (hull[min_x]['x']-hull[max_x]['x'])*(points[point]['y']-hull[max_x]['y'])-(points[point]['x']-hull[max_x]['x'])*(hull[min_x]['y']-hull[max_x]['y'])
-		if side > 0:
-			left_of_line[point] = points[point]
-		if side < 0:
-			right_of_line[point] = points[point]
-
-	quick_hull_find(right_of_line, max_x, min_x, hull)
-	quick_hull_find(left_of_line, min_x, max_x, hull)
-
-	return hull
-
-def quick_hull_find(sk, p, q, hull):
-	if len(sk) == 0:
-		return
-
-	p_point = hull[p]
-	q_point = hull[q]
-
-	farthest_point = max(sk, key=lambda point: math.sqrt(((q_point['y']-p_point['y'])*(sk[point]['x']-p_point['x'])+(q_point['x']-p_point['x'])*(sk[point]['y']-p_point['y']))**2/((q_point['x']-p_point['x'])**2 + (q_point['y']-p_point['y'])**2)))
-
-	hull[farthest_point] = sk.pop(farthest_point)
-	c_point = hull[farthest_point]
-	s1 = {}
-	s2 = {}
-
-	for point in sk:
-		pc_side = (c_point['x']-p_point['x'])*(sk[point]['y']-p_point['y'])-(sk[point]['x']-p_point['x'])*(c_point['y']-p_point['y'])
-		cq_side = (q_point['x']-c_point['x'])*(sk[point]['y']-c_point['y'])-(sk[point]['x']-c_point['x'])*(q_point['y']-c_point['y'])
-		if pc_side < 0:
-			s1[point] = sk[point]
-		if cq_side < 0:
-			s2[point] = sk[point]
-
-	quick_hull_find(s1, p, farthest_point, hull)
-	quick_hull_find(s2, farthest_point, q, hull)
 
 
 if __name__ == '__main__':
